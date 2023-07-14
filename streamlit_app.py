4c4e75d1fc6e66a0bdcf375f77d178ad6a8458f9
HarshAgarwal312 <73429823+HarshAgarwal312@users.noreply.github.com>
2023-07-14 12:44:45 +0530

    Update streamlit_app.py
    made changes to test alignment or presentation of data on app

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,14 @@ import pandas
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
 streamlit.title("My Parents New Healthy Diner")
-streamlit.dataframe(my_fruit_list)
 streamlit.header("Breakfast Menu")
 streamlit.text("🥣 Omega 3 and Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+streamlit.dataframe(my_fruit_list)
 
 
 #streamlit.subheader("Breakfast Menu")
