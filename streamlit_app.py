c60d6b0d3ac3c15749afc14637079f64b07e3f30
HarshAgarwal312 <73429823+HarshAgarwal312@users.noreply.github.com>
2023-07-14 11:54:15 +0530

    Update streamlit_app.py
    error because .markdown('\n',join()) but actualy .markdown('\n'.join())

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.title("My Parents New Healthy Diner")
 streamlit.subheader("Breakfast Menu")
 
 items = ['Omega 3 and Blueberry Oatmeal','Kale, Spinach & Rocket Smoothie','Hard-Boiled Free-Range Egg']
-streamlit.markdown('\n',join(items))
+streamlit.markdown('\n'.join(items))
