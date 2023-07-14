308ae9af2051a10208b6ef8244c2e28e4866e0c5
HarshAgarwal312 <73429823+HarshAgarwal312@users.noreply.github.com>
2023-07-14 11:51:10 +0530

    Update streamlit_app.py
    added a subheader below title
    added items and used write()/.markdown() to form a list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
+streamlit.subheader("Breakfast Menu")
+
+items = ['Omega 3 and Blueberry Oatmeal','Kale, Spinach & Rocket Smoothie','Hard-Boiled Free-Range Egg']
+streamlit.write(items)
