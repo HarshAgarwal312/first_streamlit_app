e82428465e10700aac0d0459627c65a27d2767ff
HarshAgarwal312 <73429823+HarshAgarwal312@users.noreply.github.com>
2023-07-14 11:52:38 +0530

    Update streamlit_app.py
    used .markdown('\n',join()) for displaying list under subheader

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.title("My Parents New Healthy Diner")
 streamlit.subheader("Breakfast Menu")
 
 items = ['Omega 3 and Blueberry Oatmeal','Kale, Spinach & Rocket Smoothie','Hard-Boiled Free-Range Egg']
-streamlit.write(items)
+streamlit.markdown('\n',.join(items))
