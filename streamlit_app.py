997124b0ff0999bc26debf0ae24104988647d246
HarshAgarwal312 <73429823+HarshAgarwal312@users.noreply.github.com>
2023-07-14 14:00:31 +0530

    Update streamlit_app.py
    fruits_selected list from multiselect
    fruits_to_show from fruits_selected
    displaying selected fruits row

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,10 @@ streamlit.dataframe(my_fruit_list)
 #streamlit.multiselect("Pick fruits of your choice: ",list(my_fruit_list['Fruit']))
 streamlit.multiselect("Pick fruits of your choice: ",list(my_fruit_list.index),['Avocado','Strawberries'])
 
+fruits_selected = streamlit.multiselect("Pick fruits of your choice: ",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
+
 #streamlit.subheader("Breakfast Menu")
 #items = ['Omega 3 and Blueberry Oatmeal','Kale, Spinach & Rocket Smoothie','Hard-Boiled Free-Range Egg']
 #streamlit.markdown("\n".join(items))
