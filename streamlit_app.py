580e2e31eafa2b94cc25c78d48e67be4ee6d9d3d
HarshAgarwal312 <73429823+HarshAgarwal312@users.noreply.github.com>
2023-07-14 12:52:14 +0530

    Update streamlit_app.py
    forrgot to add column of fruits to be selected and was displaying header column as selection choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 streamlit.dataframe(my_fruit_list)
 #Put up a multiselect/ pick list for users to pick fruits they want
-streamlit.multiselect("Pick fruits of your choice: ",list(my_fruit_list))
+streamlit.multiselect("Pick fruits of your choice: ",list(my_fruit_list['Fruit']))
 
 #streamlit.subheader("Breakfast Menu")
 #items = ['Omega 3 and Blueberry Oatmeal','Kale, Spinach & Rocket Smoothie','Hard-Boiled Free-Range Egg']
