c1424e46e640009d8491b0942bc44ebe3175ee0b
HarshAgarwal312 <73429823+HarshAgarwal312@users.noreply.github.com>
2023-07-17 12:49:10 +0530

    Update streamlit_app.py
    removed json text line
    separated bae url from name

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ streamlit.dataframe(fruits_to_show)
 
 #requests/api part
 streamlit.header('FruityVice Fruit Advice')
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get('https://fruityvice.com/api/fruit/'+'watermelon')
+#streamlit.text(fruityvice_response.json()) as data is now in df form
 fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalize)
 
