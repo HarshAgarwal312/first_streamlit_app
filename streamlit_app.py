1770309c75115a6b24959e456f90d7facde8dcfe
HarshAgarwal312 <73429823+HarshAgarwal312@users.noreply.github.com>
2023-07-18 12:28:58 +0530

    Update streamlit_app.py
    added function to else part and replaced with function calling

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #requests/api part
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get('https://fruityvice.com/api/fruit/'+ fruit_choice)
+  #streamlit.text(fruityvice_response.json()) as data is now in df form
+  fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalize
 
 #streamlit.write('User wants to know about: ',fruit_choice)
 streamlit.header('FruityVice Fruit Advice')
@@ -35,10 +40,9 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:  
-    fruityvice_response = requests.get('https://fruityvice.com/api/fruit/'+ fruit_choice)
-    #streamlit.text(fruityvice_response.json()) as data is now in df form
-    fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalize)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+    
 except URLError as e:
   streamlit.error()
 
